7387c4179e8f9b922d3d16e9ead0904dd61edbca
animeboy87 <animeoath@gmail.com>
2024-08-15 14:22:55 +0530

    Update Prac_1_Eigenvactor_multi.py

--- Prac_1_Eigenvactor_multi.py
+++ Prac_1_Eigenvactor_multi.py
@@ -1,55 +1,14 @@
-# Practical 5B
-# Evaluating feed forward deep network for multiclass classification using KFold cross-validation
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.datasets import load_iris
-from sklearn.model_selection import KFold
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-from sklearn.metrics import accuracy_score, confusion_matrix
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-iris = load_iris()
-X = iris.data
-y = iris.target
-label_encoder=LabelEncoder()
-integer_encoded = label_encoder.fit_transform(y)
-onehot_encoder = OneHotEncoder(sparse=False)
-integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
-y_encoded = onehot_encoder.fit_transform(integer_encoded)
-k_folds = 5
-kf = KFold(n_splits=k_folds, shuffle=True, random_state=42)
-def create_model():
-  model = Sequential()
-  model.add(Dense(10, input_dim=4, activation= 'relu'))
-  model.add(Dense(8, activation='relu'))
-  model.add(Dense(3, activation='softmax'))
-  model.compile(loss = 'categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-  return model
-fold = 0
-accuracies = []
-conf_matrices = []
-for train_index, test_index in kf.split(X):
-  fold += 1
-  X_train, X_test = X[train_index], X[test_index]
-  y_train, y_test = y_encoded[train_index], y_encoded[test_index]
-  model = create_model()
-  model.fit(X_train, y_train, epochs=100, batch_size=5, verbose=0)
-  y_pred = model.predict(X_test)
-  y_pred_classes = np.argmax(y_pred, axis=1)
-  y_test_classes = np.argmax(y_test, axis=1)
-  accuracy = accuracy_score(y_test_classes, y_pred_classes)
-  accuracies.append(accuracy)
-  conf_matrix = confusion_matrix(y_test_classes, y_pred_classes)
-  conf_matrices.append(conf_matrix)
-  print(f"Fold {fold} - Accuracy: {accuracy}")
-  print("Confusion Matrix: ")
-  print(conf_matrix)
-  print()
-avg_accuracy = np.mean(accuracies)
-print(f'Average Accuracy: {avg_accuracy}')
-plt.figure(figsize=(8, 6))
-sns.boxplot(y=accuracies)
-plt.title('Distribution of Accuracies Across Folds')
-plt.xlabel('Accuracy')
-plt.show()
+# Practical 1
+# Performing matrix multiplication and finding eigenvectors and eigenvalues using TensorFlow.
+import tensorflow as tf
+print("Matrix Multiplication")
+x=tf.constant([1,2,3,4,5,6],shape=[2,3])
+print(x)
+y=tf.constant([7,8,9,10,11,12],shape=[3,2])
+print(y)
+z=tf.matmul(x,y)
+print("Product:",z)
+e_matrix_A=tf.random.uniform([2,2],minval=3,maxval=10,dtype=tf.float32,name="matrixA")
+print("Matrix A:\n{}\n\n".format(e_matrix_A))
+eigen_values_A,eigen_vectors_A=tf.linalg.eigh(e_matrix_A)
+print("Eigen Vectors of Matrix A: \n{}\n\nEigen Values of Matrix A: \n{}\n".format(eigen_vectors_A,eigen_values_A))
